b5f601aad0fc80af1326786f0ce33bb12ba47add
Nick Østergaard <oe.nick@gmail.com>
2025-01-08 23:08:26 +0100

    Try out edgar-bonet's simple CLI
    From https://gist.github.com/edgar-bonet/607b387260388be77e96

--- cdc_multi_test/src/main.cpp
+++ cdc_multi_test/src/main.cpp
@@ -44,6 +44,61 @@
 void printAll(int ch);
 boolean delay_without_delaying(unsigned long time);
 
+
+#define BUF_LENGTH 128  /* Buffer for the incoming command. */
+
+static bool do_echo = true;
+float tempC;
+void print_prompt(void)
+{
+  tempC = analogReadTemp(); // Get internal temperature
+  Serial.print(tempC);
+  Serial.print(F("ºC > "));
+}
+
+/* Execute a complete command. */
+static void exec(char *cmdline, int length)
+{
+    char *command = strsep(&cmdline, " ");
+
+    if (strcmp_P(command, PSTR("help")) == 0) {
+        Serial.println(F(
+            "mode <pin> <mode>: pinMode()\r\n"
+            "read <pin>: digitalRead()\r\n"
+            "aread <pin>: analogRead()\r\n"
+            "write <pin> <value>: digitalWrite()\r\n"
+            "awrite <pin> <value>: analogWrite()\r\n"
+            "msg <message>: Write a message to USBSer2\r\n"
+            "echo <value>: set echo off (0) or on (1)"));
+    } else if (strcmp_P(command, PSTR("mode")) == 0) {
+        int pin = atoi(strsep(&cmdline, " "));
+        int mode = atoi(cmdline);
+        pinMode(pin, mode);
+    } else if (strcmp_P(command, PSTR("read")) == 0) {
+        int pin = atoi(cmdline);
+        Serial.println(digitalRead(pin));
+    } else if (strcmp_P(command, PSTR("aread")) == 0) {
+        int pin = atoi(cmdline);
+        Serial.println(analogRead(pin));
+    } else if (strcmp_P(command, PSTR("write")) == 0) {
+        int pin = atoi(strsep(&cmdline, " "));
+        int value = atoi(cmdline);
+        digitalWrite(pin, value);
+    } else if (strcmp_P(command, PSTR("awrite")) == 0) {
+        int pin = atoi(strsep(&cmdline, " "));
+        int value = atoi(cmdline);
+        analogWrite(pin, value);
+    } else if (strcmp_P(command, PSTR("msg")) == 0) {
+        USBSer2.println(cmdline);
+    } else if (strcmp_P(command, PSTR("echo")) == 0) {
+        do_echo = atoi(cmdline);
+    } else {
+        Serial.print(F("Error: Unknown command: "));
+        Serial.println(command);
+    }
+    print_prompt();
+}
+
 void setup() {
   pinMode(LED, OUTPUT);
 
@@ -93,6 +148,7 @@ void setup() {
 int LEDstate = 0;
 
 void loop() {
+  /*
   int ch;
 
   ch = Serial.read();
@@ -104,12 +160,48 @@ void loop() {
   if (ch > 0) {
     printAll(ch);
   }
-
+*/
   if (delay_without_delaying(500)) {
     LEDstate = !LEDstate;
     digitalWrite(LED, LEDstate);
   }
 }
+
+
+void setup1() {}
+void loop1() {
+
+    /* Process incoming commands. */
+    while (Serial.available()) {
+        static char buffer[BUF_LENGTH];
+        static int length = 0;
+
+        int data = Serial.read();
+        if (data == '\b' || data == '\177') {  // BS and DEL
+            if (length) {
+                length--;
+                if (do_echo) Serial.write("\b \b");
+            }
+        }
+        else if (data == '\r') {
+            if (do_echo) Serial.write("\r\n");    // output CRLF
+            buffer[length] = '\0';
+            if (length) {
+              exec(buffer, length);
+            } else {
+              print_prompt();
+            }
+            length = 0;
+        }
+        else if (length < BUF_LENGTH - 1) {
+            buffer[length++] = data;
+            if (do_echo) Serial.write(data);
+        }
+    }
+
+    /* Whatever else needs to be done... */
+}
+
 
 // print to all CDC ports
 void printAll(int ch) {
